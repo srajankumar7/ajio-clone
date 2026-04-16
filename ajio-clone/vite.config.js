603f5c50d94749c473e272df9fc22865777a286a
Srajan Kumar <gb.srajan7@gmail.com>
2026-04-16 16:40:21 +0530

    Add react-responsive-carousel

--- ajio-clone/vite.config.js
+++ ajio-clone/vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-responsive-carousel/lib/styles/carousel.min.css']
-    }
-  }
 })
