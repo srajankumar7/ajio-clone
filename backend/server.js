e237b80308a3c273ed3835a0ef43084cee6e5563
Srajan Kumar <gb.srajan7@gmail.com>
2026-04-22 17:28:36 +0530

    Cloudinary setup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
-const multer = require("multer");
 const path = require("path");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
