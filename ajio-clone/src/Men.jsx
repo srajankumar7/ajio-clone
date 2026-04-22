b6c4ef0205f70f5f68428691ad4bd7fde8041903
Srajan Kumar <gb.srajan7@gmail.com>
2026-04-22 11:07:01 +0530

    Fix carousel not showing

--- ajio-clone/src/Men.jsx
+++ ajio-clone/src/Men.jsx
@@ -1,32 +1,44 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { Carousel } from 'react-responsive-carousel';
-function Men() {
-    const images = [
-        {
-            src: "/assets/img5.jpg",
-            alt: "Image 1"
-        },
-        {
-            src: "/assets/img6.jpg",
-            alt: "Image 2"
-        },
-        {
-            src: "/assets/img4.jpg",
-            alt: "Image 3"
-        }];
-    return (
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
 
-        <div className='section' >
-            <h1 style={{textAlign: "center"}}>Men's page</h1>
-                            <Carousel classname="rounded-xl"autoPlay infiniteLoop interval={2000}
-                                showArrows showThumbs={false} showStatus={false} showIndicators={true}>
-                    {
-                        images.map(image => (
-                        <img  src={image.src} alt={image.alt} />
-                    ))}     
-                </Carousel>
-            </div>
-    );
+function Men() {
+  const images = [
+      {
+          src: "/assets/img5.jpg",
+          alt: "Image 1"
+      },
+      {
+          src: "/assets/img6.jpg",
+          alt: "Image 2"
+      },
+      {
+          src: "/assets/img4.jpg",
+          alt: "Image 3"
+      }
+  ];
+
+  return (
+    <div className="section">
+      <h1 style={{ textAlign: "center" }}>Men's page</h1>
+
+      <Carousel
+        className="rounded-xl"
+        autoPlay
+        infiniteLoop
+        interval={2000}
+        showArrows
+        showThumbs={false}
+        showStatus={false}
+        showIndicators={true}
+      >
+        {images.map((image, index) => (
+          <div key={index}>
+            <img src={image.src} alt={image.alt} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
 }
+
 export default Men;
