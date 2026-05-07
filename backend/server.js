53ef54aa7b12814f3f834c3da41c78deecba8de2
Srajan Kumar <gb.srajan7@gmail.com>
2026-05-07 13:52:03 +0530

    fixed  mail final

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,7 @@ const fs = require("fs");
 
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS
